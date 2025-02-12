fe9a9367edd0a9f611e5985866b70b04cd14b0ea
arunimac02 <chowdh36@purdue.edu>
2025-02-12 02:11:53 -0600

    Added 4 new movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'The Shawshank Redemption', :rating => 'R',
+    :release_date => '14-Oct-1994'},
+  {:title => 'The Godfather', :rating => 'R',
+    :release_date => '24-Mar-1972'},
+  {:title => 'The Dark Knight', :rating => 'PG-13',
+    :release_date => '18-July-2008'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-July-2010'}    
 ]
 
 more_movies.each do |movie|
