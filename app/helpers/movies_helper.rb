f6c4f9efb667b75f68f5596cce2462ccd9345341
arunimac02 <chowdh36@purdue.edu>
2025-02-12 18:45:33 -0600

    Edited a few lines of code

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,5 @@
 module MoviesHelper
-    def toggle_direction(column)
+    def sorting_direction(column)
         if column == session[:sort]
           session[:direction] == 'asc' ? 'desc' : 'asc'
         else
@@ -7,7 +7,7 @@ module MoviesHelper
         end
     end
 
-    def sorted_symbol(column)
+    def symbol_direction(column)
         if @sort_column == column
           @sort_direction == 'asc' ? '↑' : '↓'
         else
